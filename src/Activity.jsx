02000699263b608c9b3e147b63becb2ec878362c
Arian Abedanzadeh <arianabedan@gmail.com>
2021-10-19 19:29:13 -0400

    Update Depenedencies and fix console errors with css addition for activities

--- src/Activity.jsx
+++ src/Activity.jsx
@@ -1,15 +1,43 @@
-import React, { useEffect, useState, Fragment} from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { History } from './components/History.jsx';
 import { getActivity } from './request.js';
 import { getParsedActivity } from './utilities.js';
 
+// const [currentTab, setCurrentTab] = useState('0');
+// const [callHistory, setCallHistory] = useState({ '0': {}, '1': {} });
+// const [loading, isLoading] = useState(true);
+
+// useEffect(() => {
+//     getActivity().then((res) => {
+//         setCallHistory(getParsedActivity(res.data));
+//         isLoading(false)
+//     }).catch(
+//         isLoading(false)
+//     );
+// }, [])
+
 const Activity = (props) => {
 
+
+
+    // const [currentTab, setCurrentTab] = useState('0');
+    // const [callHistory, setCallHistory] = useState({ '0': {}, '1': {} });
+    // const [loading, isLoading] = useState(true);
+    // const getData = () => {
+    //     getActivity().then((res)=> {
+    //         setCallHistory(getParsedActivity(res.data));
+    //         isLoading(false)
+    //     }).catch(
+    //         isLoading(false)
+    //     );
+    // }
+
     const [currentTab, setCurrentTab] = useState('0');
-    const [callHistory, setCallHistory] = useState({'0':{},'1':{}});
+    const [callHistory, setCallHistory] = useState({ '0': {}, '1': {} });
     const [loading, isLoading] = useState(true);
+
     const getData = () => {
-        getActivity().then((res)=> {
+        getActivity().then((res) => {
             setCallHistory(getParsedActivity(res.data));
             isLoading(false)
         }).catch(
@@ -19,16 +47,17 @@ const Activity = (props) => {
 
     useEffect(() => {
         getData();
-    },[])
+    }, [])
 
 
-    return(
+
+    return (
         <Fragment>
-            {!loading && <History currentTab={currentTab} setCurrentTab={setCurrentTab} callHistory={callHistory[currentTab]} getData={getData}/>}
+            {!loading && <History currentTab={currentTab} setCurrentTab={setCurrentTab} callHistory={callHistory[currentTab]} getData={getData} />}
             {loading && <div className="loader"></div>}
         </Fragment>
     )
 
 }
 
-export default Activity; 
+export default Activity;
